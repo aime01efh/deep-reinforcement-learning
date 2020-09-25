ff90acb193313bc22e4a4b5902a0392699b1564e
Ed Haymore <efh-github@haymore.com>
2020-09-25 11:52:49 -0700

    New driver baseline

--- p3_collab-compet/run.py
+++ p3_collab-compet/run.py
@@ -4,6 +4,7 @@ import maddpg_agent
 import ddpg_agent
 # import random
 
+
 env = UnityEnvironment(file_name="Tennis_Linux/Tennis.x86_64")
 # get the default brain
 brain_name = env.brain_names[0]
@@ -15,35 +16,12 @@ states = env_info.vector_observations
 state_size = states.shape[1]
 num_agents = len(env_info.agents)
 
-# if True:
-#     # Parameter defaults
-#     ddpg_params = ddpg_agent.NNParams(
-#         in_actor=num_agents * state_size,  # observations from both agents are concatenated
-#         hidden_in_actor=maddpg_train.HIDDEN_IN_ACTOR,
-#         hidden_out_actor=maddpg_train.HIDDEN_OUT_ACTOR,
-#         out_actor=action_size,
-#         in_critic=num_agents * state_size + num_agents * action_size,
-#         hidden_in_critic=maddpg_train.HIDDEN_IN_CRITIC,
-#         hidden_out_critic=maddpg_train.HIDDEN_OUT_CRITIC,
-#         lr_actor=maddpg_train.LR_ACTOR,
-#         lr_critic=maddpg_train.LR_CRITIC,
-#     )
-#     print(ddpg_params)
-
-#     agent = maddpg_agent.MADDPG_Agent(
-#         num_agents=num_agents,
-#         ddpg_params=ddpg_params,
-#         discount_factor=maddpg_train.DISCOUNT_FACTOR,
-#         tau=maddpg_train.TAU,
-#     )
-
-#     score_history = []
-#     maddpg_train.train_maddpg(
-#         env, agent, report_every=20, score_history=score_history,
-#     )
+# If you switch this, also change obs in maddpg_train.get_train_obs
+# in_actor_size = num_agents * state_size  # obs from both agents are concatenated
+in_actor_size = state_size  # each actor gets only its own observations
 
 # if True:
-#     # Random
+#     print('Random values')
 #     hidden_in_actor = random.choice([32, 64, 128, 256, 512])
 #     hidden_out_actor = random.choice([32, 64, 128, 256, 512])
 #     hidden_in_critic = random.choice([32, 64, 128, 256, 512])
@@ -52,7 +30,7 @@ num_agents = len(env_info.agents)
 #     lr_critic = random.choice([1e-5, 3e-5, 1e-4, 3e-4, 1e-3])
 
 #     ddpg_params = ddpg_agent.NNParams(
-#         in_actor=num_agents * state_size,  # observations from both agents are concatenated
+#         in_actor=in_actor_size,
 #         hidden_in_actor=hidden_in_actor,
 #         hidden_out_actor=hidden_out_actor,
 #         out_actor=action_size,
@@ -70,7 +48,7 @@ num_agents = len(env_info.agents)
 #     episodes_per_update = random.choice([1, 2, 4, 8])
 #     discount_factor = random.choice([1.0, 0.99, 0.98, 0.95])
 #     tau = random.choice([1e-4, 1e-3, 1e-2])
-#     ou_noise = random.choice([0.5, 1.0, 2.0, 4.0])
+#     ou_noise = random.choice([1.0, 2.0, 4.0])
 #     noise_reduction = random.choice([0.99999, 0.9999, 0.999])
 #     print(f'batchsize={batchsize}, '
 #           f'replay_buffer_len={maddpg_train.REPLAY_BUFFER_LEN}, '
@@ -88,7 +66,7 @@ num_agents = len(env_info.agents)
 
 #     score_history = []
 #     maddpg_train.train_maddpg(
-#         env, agent, report_every=200, num_episodes=7000,
+#         env, agent, report_every=200, num_episodes=5000,
 #         score_history=score_history,
 #         batchsize=batchsize,
 #         episode_length=episode_length,
@@ -100,15 +78,15 @@ num_agents = len(env_info.agents)
 
 if True:
     # Test values
-    hidden_in_actor = 32
+    hidden_in_actor = 128
     hidden_out_actor = 256
-    hidden_in_critic = 64
+    hidden_in_critic = 256
     hidden_out_critic = 512
-    lr_actor = 3e-5
+    lr_actor = 1e-5
     lr_critic = 1e-4
 
     ddpg_params = ddpg_agent.NNParams(
-        in_actor=num_agents * state_size,  # observations from both agents are concatenated
+        in_actor=in_actor_size,
         hidden_in_actor=hidden_in_actor,
         hidden_out_actor=hidden_out_actor,
         out_actor=action_size,
@@ -121,13 +99,13 @@ if True:
     print(ddpg_params)
 
     batchsize = 128
-    maddpg_train.REPLAY_BUFFER_LEN = 1000
-    episode_length = 80
-    episodes_per_update = 1
-    discount_factor = 0.99
+    maddpg_train.REPLAY_BUFFER_LEN = 1_000_000
+    episode_length = 1000
+    episodes_per_update = 2
+    discount_factor = 0.99  # gamma
     tau = 1e-4
-    ou_noise = 4.0
-    noise_reduction = 0.9999
+    ou_noise = 2.0
+    noise_reduction = 0.999
     print(f'batchsize={batchsize}, '
           f'replay_buffer_len={maddpg_train.REPLAY_BUFFER_LEN}, '
           f'episode_length={episode_length}, '
@@ -144,7 +122,7 @@ if True:
 
     score_history = []
     maddpg_train.train_maddpg(
-        env, agent, report_every=200, num_episodes=100000,
+        env, agent, report_every=200, num_episodes=10000,
         score_history=score_history,
         batchsize=batchsize,
         episode_length=episode_length,
