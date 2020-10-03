71f3b757875fabe8e527fb2014a2daca0e8f6c8c
Ed Haymore <efh-github@haymore.com>
2020-10-02 21:02:01 -0700

    Adjust drivers

--- p3_collab-compet/run_random.py
+++ p3_collab-compet/run_random.py
@@ -2,6 +2,10 @@ from unityagents import UnityEnvironment
 import maddpg_train
 import maddpg_agent
 import ddpg_agent
+import random
+import datetime
+import pathlib
+import numpy as np
 
 
 env = UnityEnvironment(file_name="Tennis_Linux/Tennis.x86_64", no_graphics=True)
@@ -19,16 +23,14 @@ num_agents = len(env_info.agents)
 # in_actor_size = num_agents * state_size  # obs from both agents are concatenated
 in_actor_size = state_size  # each actor gets only its own observations
 
-
-# Test values
-in_actor_size = 24
-hidden_in_actor = 256
-hidden_out_actor = 128
-hidden_in_critic = 256
-hidden_out_critic = 256
-lr_actor = 5e-4
-lr_critic = 1e-3
-dropout = 0.2
+print("Random values")
+hidden_in_actor = random.choice([128, 256])
+hidden_out_actor = random.choice([128, 256])
+hidden_in_critic = random.choice([256, 512])
+hidden_out_critic = random.choice([256, 512])
+lr_actor = random.choice([1e-5, 1e-4, 1e-3])
+lr_critic = random.choice([1e-5, 1e-4, 1e-3])
+dropout = random.choice([0.1, 0.2, 0.3, 0.4])
 
 ddpg_params = ddpg_agent.NN_Params(
     in_actor=in_actor_size,
@@ -45,16 +47,16 @@ ddpg_params = ddpg_agent.NN_Params(
 )
 print(ddpg_params)
 
-batchsize = 512
-episode_length = 512
+batchsize = random.choice([256, 512, 1024])
+episode_length = random.choice([200, 500, 1000])
 update_step_interval = 1
-update_iterations = 1
-discount_factor = 0.99
-tau = 1e-3
-initial_noise_scale = 5.0
-min_noise_scale = 0.0
-episode_noise_end = 300
-replay_buffer_len = 1_000_000
+update_iterations = random.choice([1, 3, 5])
+discount_factor = random.choice([1.0, 0.999, 0.99, 0.98])
+tau = random.choice([1e-4, 1e-3, 1e-2])
+initial_noise_scale = random.choice([1.0, 3.0, 5.0])
+min_noise_scale = random.choice([0.0, 0.01, 0.1])
+episode_noise_end = random.choice([300, 1000, 2500])
+replay_buffer_len = random.choice([500_000, 1_000_000])
 
 maddpg_params = maddpg_train.MADDPG_Params(
     batchsize=batchsize,
@@ -82,7 +84,20 @@ maddpg_train.train_maddpg(
     env,
     main_agent,
     maddpg_params=maddpg_params,
-    num_episodes=10000,
+    num_episodes=5000,
     score_history=score_history,
-    report_every=100,
+    report_every=200,
+    progressbar=False,
 )
+
+best_mean = 0.0
+WINLEN = 100
+for idx in range(len(score_history) - WINLEN):
+    this_mean = np.mean(score_history[idx : idx + WINLEN])
+    best_mean = max(best_mean, this_mean)
+
+datestr = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
+outfile = pathlib.Path("savelog") / f"{datestr}-scores.txt"
+outfile.parent.mkdir(exist_ok=True)
+outfile.write_text("\n".join([str(x) for x in score_history]) + "\n")
+print(f"Best mean {best_mean} : {outfile}")
